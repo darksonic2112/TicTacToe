477e97ec6acd131816fef0ceabef6d1d34f16bd2
DARKSONIC <kevin-lehmann99@web.de>
2023-11-11 22:34:37 +0100

    Implemented Server with threads and listener

--- Server.py
+++ Server.py
@@ -10,3 +10,41 @@ SERVER = os.getenv('IPV4')
 port = 5555
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((SERVER, port))
+except socket.error as e:
+    str(e)
+
+s.listen(2)
+print("Waiting for a connection, Server Started")
+
+
+def threaded_client(conn):
+    conn.send(str.encode("Connected"))
+    reply = ""
+    while True:
+        try:
+            data = conn.recv(2048)
+            reply = data.decode("utf-8")
+
+            if not data:
+                print("Disconnected")
+                break
+            else:
+                print("Received: ", reply)
+                print("Sending: ", reply)
+
+            conn.sendall(str.encode(reply))
+        except:
+            break
+
+    print("Lost Connection")
+    conn.close()
+
+
+while True:
+    conn, addr = s.accept()
+    print("Connected to_: ", addr)
+
+    start_new_thread(threaded_client(conn,))
