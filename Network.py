ed5de5a8281f0db4eee23fd4cd570c5b13f1d927
DARKSONIC <kevin-lehmann99@web.de>
2023-11-16 23:29:39 +0100

    Imported pickle package and adjusted network.

--- Network.py
+++ Network.py
@@ -1,5 +1,6 @@
 import os
 import socket
+import pickle
 
 from dotenv import load_dotenv
 
@@ -19,13 +20,13 @@ class Network:
     def connect(self):
         try:
             self.client.connect(self.addr)
-            return self.client.recv(2048).decode()
+            return pickle.loads(self.client.recv(2048))
         except:
             pass
 
     def send(self, data):
         try:
-            self.client.send(str.encode(data))
-            return self.client.recv(2048).decode()
+            self.client.send(pickle.dumps(data))
+            return pickle.loads(self.client.recv(2048))
         except socket.error as e:
             print(e)
